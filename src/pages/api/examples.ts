51ee276fa31b9de15ae2e63f86696279af50f362
Nam <namdao2000@gmail.com>
2022-11-03 13:55:34 +1100

    Removed unnecessary files

--- src/pages/api/examples.ts
+++
@@ -1,8 +0,0 @@
-// src/pages/api/examples.ts
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json("Hello World");
-};
-
-export default examples;
