04dc949dc6d113a44dccd6eb0b5a208d78508e1b
Stefanie <stefcodes@gmail.com>
2023-01-03 22:26:53 -0700

    add connect Wallet

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,3 +1,31 @@
-export default function AuthProvider() {
-  return <div>TODO</div>;
+import { ReactElement, useEffect, useState } from "react";
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}): ReactElement {
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const address = useAddress();
+
+  useEffect(() => {
+    if (address) {
+      setIsLoggedin(true);
+    } else {
+      setIsLoggedin(false);
+    }
+  }, [address]);
+
+  if (!isLoggedin) {
+    return (
+  <div className={"flex h-screen w-full items-center justify-center"}>
+  <div>
+    <h1 className={"text-lg"}>Please login to see collection</h1>
+      <ConnectWallet />
+  </div>
+  </div>
+  );
+  }
+  return <>{children}</>;
 }
