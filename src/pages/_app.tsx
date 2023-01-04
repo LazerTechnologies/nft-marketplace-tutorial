7787374f3771e58df13129280bd9c1f915abd0ee
Stefanie <stefcodes@gmail.com>
2023-01-03 20:56:00 -0700

    add mumbai chaim

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+
+  );
 };
 
 export default MyApp;
