70824564715cba941aee3a490d90f238c1b31d01
Stefanie <stefcodes@gmail.com>
2023-01-03 23:19:48 -0700

    create card with NFT info

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,3 +1,45 @@
-export default function NFTCard() {
-  return <div>TODO</div>;
+import Image from "next/image"; 
+import { MediaRenderer } from "@thirdweb-dev/react";
+import NFT from "../pages/assets/[listingId]";
+import { string } from "zod";
+
+export default function NFTCard({
+  nft,
+}: {
+  nft: {
+    tokenUri: string;
+    name: string;
+    price?: string;
+  };
+}) {
+  return (
+    <div
+      className={`relative flex cursor-pointer 
+      flex-col overflow-hidden rounded-lg bg-white shadow-lg 
+      transition-all duration-300 hover:shadow-2xl dark:bg-[#333333]`}
+    >
+      <MediaRenderer
+        src={nft.tokenUri}
+        style={{
+          objectFit: "cover",
+        }}
+        className={
+          "h-[244px] rounded-lg transition duration-300 ease-in-out hover:scale-105"
+        }
+      />
+      <div className={`flex flex-col gap-y-3 p-3`}>
+        <div className={`text-sm font-semibold`}>{nft.name}</div>
+
+        {nft.price && (
+          <div>
+            <div className={`text-xs font-semibold`}>Price</div>
+            <div className={`flex items-center gap-x-1`}>
+              <Image src={"/matic-logo.png"} height={16} width={16} />
+              <p className={`text-base font-semibold`}>{nft.price}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
