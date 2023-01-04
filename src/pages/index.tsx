88bc91c2f78f8104471035145c568dd925bf98d8
Stefanie <stefcodes@gmail.com>
2023-01-03 23:19:18 -0700

    add contract

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { useActiveListings, useContract } from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
-  return <div>TODO</div>;
+  const {contract} = useContract(
+    "0x7653Cd64320c65733C005EF855CdE916705B483D",
+    "marketplace"
+  );
+
+  const { data, isLoading} = useActiveListings(contract); 
+  console.log(data);
+
+  return (
+    <div>BEAT Marketplace</div>
+  );
 };
 
 export default Home;
